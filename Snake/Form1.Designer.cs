a47d2b1bfdf23a29267846d36d67b79dece2c55a
hlinacze <hliba.ondra@live.com>
2017-10-17 15:01:14 +0200

    Add all classes and properties for snake class

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1406, 676);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1406, 676);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
     }
 }
 
---
+++ Snake/Had.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Had
+    {
+        private Hlava hlava;
+        private List<CastHada> castiTela;
+
+        private Smer smer;
+        private int rychlost;
+        private int skore;
+
+        public Had()
+        {
+            hlava = new Hlava();
+            castiTela = new List<CastHada>();
+            for (int i = 0; i < 3; i++)
+            {
+                castiTela.Add(new CastHada());
+            }
+            smer = Smer.Doprava;
+            rychlost = 1;
+            skore = 0;
+        }
+    }
+
+    public enum Smer
+    {
+        Nahoru,
+        Doprava,
+        Dolu,
+        Doleva
+    }
+}
